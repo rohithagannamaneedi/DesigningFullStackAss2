a4d2012b4ad36baf196d814ddef312c9d4624d53
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-04-11 13:52:36 +0530

    readme

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
 // write button card here
+// write button card here
+import React from 'react'
+
+const button = () => {
+  return (
+    <button>
+        View Product
+    </button>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+// write product card here
+import React from 'react'
+import Button from './button';
+
+const productcard = () => {
+
+    const productImage = "https://www.tinyminymo.com/cdn/shop/files/Harry-Potter-Action-Figure-5.jpg?v=1741539516&width=720";
+    const productName = "Harry Potter Figures";
+    const productPrice = "$50" ;
+  return (
+    <div className='productcard'>
+        <img src={productImage} alt="Image" />
+        <p>{productName}</p>
+        <p>{productPrice}</p>
+        <Button/>
+      
+    </div>
+  )
+}
+
+export default productcard
